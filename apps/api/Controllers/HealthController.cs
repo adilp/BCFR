f91d12d0a2af713913e011fc4605098b19c65e96
adilp <8204127+adilp@users.noreply.github.com>
2025-08-11 11:19:33 -0500

    digital ocean prefix issue. stripped /api

--- apps/api/Controllers/HealthController.cs
+++ apps/api/Controllers/HealthController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace MemberOrgApi.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("[controller]")]
 public class HealthController : ControllerBase
 {
     [HttpGet]
